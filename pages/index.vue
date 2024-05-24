01015d9d264729117a3fc4438a2ea7786111682a
Sapozhnyk Dmytro <dimonsuper010@gmail.com>
2024-05-24 20:01:25 +0300

    Add Button component to Rewards history section

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@ import { useAccount } from 'use-wagmi';
 import { config } from '@/config/wagmi.config';
 import Card from '~/components/ui-kit/Card.vue';
 import Ticker from '~/components/ui-kit/Ticker.vue';
+import Button from '~/components/ui-kit/Button.vue';
 
 const userStore = useUserStore();
 const { address } = storeToRefs(userStore);
@@ -36,6 +37,11 @@ onMounted(() => {
 });
 
 initRelayRegistry();
+
+const handleClaimAllRewards = async () => {
+  debugger;
+  await userStore.claimAllRewards();
+};
 </script>
 
 <template>
@@ -62,46 +68,53 @@ initRelayRegistry();
       </DashboardMobileSection>
 
       <DashboardMobileSection class="lg:basis-1/2" title="my-rewards">
-        <Card title="Rewards history" :icon="'eos-icons:trusted-organization'">
-          <p class="mb-4 text-sm">
-            Earn rewards by contributing relays to the ATOR network.
-          </p>
-          <div class="my-4 flex flex-col border-l-4 border-cyan-600 pl-3">
-            <h3>Claimed rewards</h3>
-            <div class="inline-flex items-baseline gap-2">
-              <span v-if="isConnected" class="text-4xl font-bold">
-                {{ userStore.claimedRewardsTotal }}
-              </span>
-              <span v-if="!isConnected" class="text-4xl font-bold"> -- </span>
-              <Ticker />
+        <Card>
+          <div class="flex justify-between items-center">
+            <div>
+              <h2
+                class="dark:text-cyan-200 lg:text-3xl text-2xl tracking-wide flex items-center gap-2 font-brand"
+              >
+                <Icon name="eos-icons:trusted-organization" />
+                Rewards history
+              </h2>
+              <p class="mb-4 text-sm mt-4">
+                Earn rewards by contributing relays to the ATOR network.
+              </p>
+            </div>
+
+            <Button
+              :disabled="userStore.hasClaimableRewards"
+              @onClick="handleClaimAllRewards"
+            >
+              <span v-if="userStore.hasClaimableRewards"
+                >Redeem rewards now</span
+              >
+              <span v-else>Nothing to claim</span>
+            </Button>
+          </div>
+
+          <div class="flex gap-32">
+            <div class="my-4 flex flex-col border-l-4 border-cyan-600 pl-3">
+              <h3>Claimed rewards</h3>
+              <div class="inline-flex items-baseline gap-2">
+                <span v-if="isConnected" class="text-4xl font-bold">
+                  {{ userStore.claimedRewardsTotal }}
+                </span>
+                <span v-if="!isConnected" class="text-4xl font-bold"> -- </span>
+                <Ticker />
+              </div>
+            </div>
+            <div class="my-4 flex flex-col border-l-4 border-cyan-600 pl-3">
+              <h3>Claimable rewards</h3>
+              <div class="inline-flex items-baseline gap-2">
+                <span v-if="isConnected" class="text-4xl font-bold">
+                  {{ userStore.claimableRewards }}
+                </span>
+                <span v-if="!isConnected" class="text-4xl font-bold"> -- </span>
+                <Ticker />
+              </div>
             </div>
           </div>
-
-          <div class="my-4 flex flex-col border-l-4 border-cyan-600 pl-3">
-            <h3>Claimable rewards</h3>
-            <div class="inline-flex items-baseline gap-2">
-              <span v-if="isConnected" class="text-4xl font-bold">
-                {{ userStore.claimableRewards }}
-              </span>
-              <span v-if="!isConnected" class="text-4xl font-bold"> -- </span>
-              <Ticker />
-            </div>
-          </div>
-
-          <div class="mt-4 p-1 text-xs text-gray-500">
-            Last Updated: {{ lastClaimedTimestamp ?? new Date().toUTCString() }}
-          </div>
-
-          <div
-            class="my-4 h-px w-full bg-gradient-to-r from-gray-600/10 via-cyan-900 to-gray-600/10"
-          ></div>
-          <ButtonAttention
-            :disabled="!userStore.hasClaimableRewards"
-            @click="userStore.claimAllRewards"
-          >
-            <span v-if="!userStore.hasClaimableRewards">Nothing to redeem</span>
-            <span v-else>Redeem rewards now ( <UserClaimableRewards />) </span>
-          </ButtonAttention>
         </Card>
       </DashboardMobileSection>
     </div>
