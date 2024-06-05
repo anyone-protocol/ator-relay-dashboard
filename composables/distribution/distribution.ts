1f768a29f37e45c1447a1023710e9fdbae84761e
Dmytro Sapozhnyk <dimonsuper010@gmail.com>
2024-06-05 13:21:33 +0300

    #ATOR-305 Update source of information for claimable rewards (#30)
    * #ATOR-305 Update source of information for claimable rewards
    
    * Update registratorContract address in nuxt.config.ts

---
+++ composables/distribution/distribution.ts
@@ -0,0 +1,201 @@
+import { type Contract } from 'warp-contracts';
+import BigNumber from 'bignumber.js';
+import moment from 'moment';
+
+import { type Claimable } from '@/utils/contracts';
+import Logger from '@/utils/logger';
+import { type DistributionState } from './contract';
+import type { PreviousDistribution } from '@/types/facilitator';
+
+export class Distribution {
+  private _refreshing: boolean = false;
+  private contract: Contract<DistributionState> | null = null;
+  private _isInitialized: boolean = false;
+  private logger = new Logger('Distribution');
+
+  get isInitialized() {
+    return this._isInitialized;
+  }
+
+  initialize(contract: Contract<DistributionState>) {
+    if (this._isInitialized) {
+      return;
+    }
+
+    this.contract = contract;
+    this._isInitialized = true;
+    /* eslint-disable-next-line @typescript-eslint/no-floating-promises */
+    this.refresh();
+  }
+
+  private setRefreshing(refreshing: boolean) {
+    useFacilitatorStore().distributionRefreshing = refreshing;
+    this._refreshing = refreshing;
+  }
+
+  async refresh(): Promise<void> {
+    try {
+      if (this._refreshing) {
+        return;
+      }
+
+      this.setRefreshing(true);
+      const auth = useUserStore();
+      this.logger.info(
+        auth.userData?.address
+          ? `Distribution refreshing for ${auth.userData?.address}`
+          : 'Distribution refreshing'
+      );
+      this.logger.time();
+
+      let claimableAtomicTokens = null;
+      if (auth.userData) {
+        claimableAtomicTokens = await this.claimable(
+          auth.userData.address as string
+        );
+      }
+      await this.getPreviousDistributions();
+      const distributionRatePerDay = await this.getDistributionRatePer('day');
+      this.logger.timeEnd();
+      this.logger.info('Distribution refreshed', {
+        claimableAtomicTokens,
+        distributionRatePerDay: distributionRatePerDay.toString(),
+      });
+      this.setRefreshing(false);
+    } catch (error) {
+      this.logger.error('ERROR REFRESHING DISTRIBUTION', error);
+    }
+  }
+
+  async getDistributionRatePer(period: 'second' | 'hour' | 'day' = 'day') {
+    if (!this.contract) {
+      throw new Error('Distribution Contract not initialized!');
+    }
+
+    const {
+      cachedValue: { state },
+    } = await this.contract.readState();
+
+    const wholeTokensPerSecond = BigNumber(
+      state.tokensDistributedPerSecond
+    ).dividedBy(1e18);
+
+    switch (period) {
+      case 'second':
+        return wholeTokensPerSecond;
+      case 'hour':
+        return wholeTokensPerSecond.times(24 * 60);
+      case 'day':
+        const rate = wholeTokensPerSecond.times(24 * 60 * 60);
+        useFacilitatorStore().distributionRatePerDay = rate.toString();
+        return rate;
+    }
+  }
+
+  async getPreviousDistributions(): Promise<PreviousDistribution[]> {
+    if (!this.contract) {
+      throw new Error('Distribution Contract not initialized!');
+    }
+
+    const {
+      cachedValue: { state },
+    } = await this.contract.readState();
+
+    let sumOfTotalDistributions = BigNumber(0);
+    const previousDistributions = Object.keys(state.previousDistributions)
+      .reverse()
+      .map<PreviousDistribution>((timestamp) => {
+        const {
+          totalScore,
+          totalDistributed,
+          timeElapsed,
+          tokensDistributedPerSecond,
+        } = state.previousDistributions[timestamp];
+
+        sumOfTotalDistributions =
+          sumOfTotalDistributions.plus(totalDistributed);
+
+        const date = new Date(Number.parseInt(timestamp));
+        const formatDuration = (elapsed: string) => {
+          const duration = moment.duration(elapsed);
+          const d = duration.days();
+          const h = duration.hours();
+          const m = duration.minutes();
+          const s = duration.seconds();
+          const ms = duration.milliseconds();
+
+          return `${d}.${h}:${m}:${s}.${ms}`;
+        };
+        return {
+          timestamp,
+          date,
+          fromNowHumanized: moment(date).fromNow(),
+          totalScore: BigNumber(totalScore).toFormat(),
+          timeElapsed: formatDuration(timeElapsed),
+          timeElapsedHumanized: moment.duration(timeElapsed).humanize(),
+          totalDistributed: BigNumber(totalDistributed)
+            .dividedBy(1e18)
+            .toFormat(2),
+          tokensDistributedPerDay: BigNumber(tokensDistributedPerSecond)
+            .dividedBy(1e18)
+            .times(24 * 60 * 60)
+            .toFormat(2),
+        };
+      });
+
+    useFacilitatorStore().sumOfTotalDistributions =
+      sumOfTotalDistributions.toString();
+    useFacilitatorStore().previousDistributions = previousDistributions;
+
+    return previousDistributions;
+  }
+
+  async claimable(address: string, humanize = false) {
+    if (!this.contract) {
+      throw new Error('Distribution Contract not initialized!');
+    }
+
+    try {
+      const { result: claimable } = await this.contract.viewState<
+        Claimable,
+        string
+      >({
+        function: 'claimable',
+        address,
+      });
+
+      const auth = useUserStore();
+      if (auth.userData && address === auth.userData.address) {
+        useFacilitatorStore().claimableAtomicTokens = claimable;
+      }
+
+      return humanize
+        ? BigNumber(claimable || 0)
+            .dividedBy(1e18)
+            .toFormat(4)
+        : claimable;
+    } catch (error) {
+      this.logger.error(
+        'Error in Distribution when checking claimable tokens for',
+        address,
+        error
+      );
+    }
+  }
+}
+
+const distribution = new Distribution();
+export const initDistribution = () => {
+  if (distribution.isInitialized) {
+    return;
+  }
+
+  const config = useRuntimeConfig();
+  const warp = useWarp();
+  const contract = warp.contract<DistributionState>(
+    config.public.distributionContract as string
+  );
+
+  distribution.initialize(contract);
+};
+export const useDistribution = () => distribution;
