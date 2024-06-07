3dc5f5085371e86799fddaf4e92e79b963122d7d
Sapozhnyk Dmytro <dimonsuper010@gmail.com>
2024-06-07 11:56:35 +0300

    Try to fix arweave host

--- composables/arweave.ts
+++ composables/arweave.ts
@@ -1,10 +1,11 @@
 import Arweave from 'arweave';
 
 // NB: Use arweave mainnet locally
-const arweaveConfig =
-  window.location.hostname === 'localhost'
-    ? { protocol: 'https', host: 'arweave.net', port: 443 }
-    : {};
+// const arweaveConfig =
+//   window.location.hostname !== 'localhost'
+//     ? { protocol: 'https', host: 'arweave.net', port: 443 }
+//     : {};
+const arweaveConfig = { protocol: 'https', host: 'arweave.net', port: 443 };
 
 const arweave = Arweave.init(arweaveConfig);
 
