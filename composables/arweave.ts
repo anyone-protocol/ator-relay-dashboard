ae62585fcaa17d990b9bdf65f0d60d7d9a0211f4
Sapozhnyk Dmytro <dimonsuper010@gmail.com>
2024-06-07 13:07:28 +0300

    Revert "Try to fix arweave host"
    This reverts commit 3dc5f5085371e86799fddaf4e92e79b963122d7d.

--- composables/arweave.ts
+++ composables/arweave.ts
@@ -1,11 +1,10 @@
 import Arweave from 'arweave';
 
 // NB: Use arweave mainnet locally
-// const arweaveConfig =
-//   window.location.hostname !== 'localhost'
-//     ? { protocol: 'https', host: 'arweave.net', port: 443 }
-//     : {};
-const arweaveConfig = { protocol: 'https', host: 'arweave.net', port: 443 };
+const arweaveConfig =
+  window.location.hostname === 'localhost'
+    ? { protocol: 'https', host: 'arweave.net', port: 443 }
+    : {};
 
 const arweave = Arweave.init(arweaveConfig);
 
