c9635ec5494ee09dd25824c1df462f052f0cf68e
MarcoMandar <malicemandar@gmail.com>
2024-06-22 23:21:42 +0100

    disable mainnet, create modal for disconnect, add switch network

--- config/wagmi.config.ts
+++ config/wagmi.config.ts
@@ -2,11 +2,16 @@ import { reconnect } from '@wagmi/core';
 import { defaultWagmiConfig } from '@web3modal/wagmi/vue';
 import { metadata } from './web3modal.config';
 import { mainnet, sepolia } from 'viem/chains';
+import { http } from '@wagmi/vue';
 
 const projectId = 'f5e29d36441ccd0e2f5e3473d5a2021b';
 
 export const config = defaultWagmiConfig({
-  chains: [mainnet, sepolia],
+  chains: [/*mainnet,*/ sepolia],
+  transports: {
+    // [mainnet.id]: http(),
+    [sepolia.id]: http(),
+  },
   projectId,
   metadata,
   ssr: false,
