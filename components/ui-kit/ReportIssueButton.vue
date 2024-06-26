4fd3ae90916f9c69260646469e9165639f864ed8
MarcoMandar <malicemandar@gmail.com>
2024-06-26 01:48:35 +0100

    contract links

--- components/ui-kit/ReportIssueButton.vue
+++ components/ui-kit/ReportIssueButton.vue
@@ -1,14 +1,8 @@
 <template>
-  <UButton
-    variant="outline"
-    @click="onReportIssueClicked"
-    class=" flex p-2 gap-3 items-center text-left cursor-pointer hover:bg-slate-100 hover:text-white dark:hover:bg-cyan-600 dark:hover:text-white"
-  >
-  <UIcon
-      name="i-heroicons-exclamation-circle"
-    />
-    <div class="flex flex-col flex-wrap gap-1">
-      <span class="dark:text-white text-xs uppercase text-black">Report Issue</span>
+  <UButton variant="outline" @click="onReportIssueClicked">
+    <UIcon name="i-heroicons-exclamation-circle" />
+    <div>
+      <span>Report Issue</span>
     </div>
   </UButton>
 </template>
