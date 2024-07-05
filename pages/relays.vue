04a4768c47962d093728220eac3539e2be3aa576
MarcoMandar <malicemandar@gmail.com>
2024-07-05 20:43:35 +0100

    split to 2 pages, add menu
    Signed-off-by: MarcoMandar <malicemandar@gmail.com>

---
+++ pages/relays.vue
@@ -0,0 +1,25 @@
+<template>
+  <DashboardMobileSection title="my-relays">
+    <Card title="Relays" :icon="'eos-icons:product-classes-outlined'">
+      <DataTableMyRelays />
+    </Card>
+  </DashboardMobileSection>
+  <ReportIssueDialog />
+  <SupportIssueDialog />
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useUserStore } from '@/stores/useUserStore';
+import DashboardMobileSection from '@/components/DashboardMobileSection.vue';
+import DataTableMyRelays from '@/components/DataTableMyRelays/DataTableMyRelays.vue';
+import Card from '@/components/ui-kit/Card.vue';
+import ReportIssueDialog from '@/components/ui-kit/ReportIssueDialog.vue';
+
+const userStore = useUserStore();
+
+onMounted(async () => {
+  await userStore.getVerifiedRelays();
+  await userStore.getClaimableRelays();
+});
+</script>
