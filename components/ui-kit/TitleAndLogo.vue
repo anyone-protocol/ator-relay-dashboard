8fe9512ca62afcd31d550224ceda2e17cfd4ccea
MarcoMandar <malicemandar@gmail.com>
2024-07-18 22:18:49 +0100

    remove logo image
    Signed-off-by: MarcoMandar <malicemandar@gmail.com>

--- components/ui-kit/TitleAndLogo.vue
+++ components/ui-kit/TitleAndLogo.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <router-link to="/" class="flex gap-2 items-top">
-      <img class="object-contain h-6 lg:h-8" src="/images/anyone-logo.png" />
+      <!-- <img class="object-contain h-6 lg:h-8" src="/images/anyone-logo.png" /> -->
       <img
-        class="object-contain h-6 lg:h-8 mt-[2px]"
+        class="object-contain h-6 lg:h-8 mt-[2px] ml-[5px]"
         src="/images/anyone-text.png"
       />
     </router-link>
