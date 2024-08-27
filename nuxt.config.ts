06f36b27ec3f8be1c1267c708daae5063e2a347e
MarcoMandar <malicemandar@gmail.com>
2024-08-27 14:36:44 +0300

    version
    Signed-off-by: MarcoMandar <malicemandar@gmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -67,7 +67,7 @@ export default defineNuxtConfig({
       supportWalletPublicKeyBase64:
         'K3jnSGVyHj4kSzgce3+k8gJsfHvUoQeJMNPO8CcsO2s=',
       commitHash: process.env.NUXT_PUBLIC_COMMIT_HASH || 'dev',
-      version: '1.1.3',
+      version: '1.1.4',
     },
   },
   plugins: [{ src: '~/plugins/vue-query.client.ts', mode: 'client' }],
