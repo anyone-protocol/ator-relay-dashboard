14503ce494cfc1b4b332ad219821a56475d04621
MarcoMandar <malicemandar@gmail.com>
2024-10-08 03:46:53 +0300

    changing no credit to Waiting...
    Signed-off-by: MarcoMandar <malicemandar@gmail.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -72,7 +72,7 @@ export default defineNuxtConfig({
       supportWalletPublicKeyBase64:
         'K3jnSGVyHj4kSzgce3+k8gJsfHvUoQeJMNPO8CcsO2s=',
       commitHash: process.env.NUXT_PUBLIC_COMMIT_HASH || 'dev',
-      version: '1.3.5',
+      version: '1.3.6',
     },
   },
   plugins: [{ src: '~/plugins/vue-query.client.ts', mode: 'client' }],
