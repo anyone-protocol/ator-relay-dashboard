a6351a30458b40ce0eeecb3a90d9bf8aa5c57394
SuchJitter <jettenmostafa2@gmail.com>
2025-03-03 11:52:07 +0100

    fixed typo and user balance

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -81,7 +81,7 @@ export default defineNuxtConfig({
       supportWalletPublicKeyBase64:
         'K3jnSGVyHj4kSzgce3+k8gJsfHvUoQeJMNPO8CcsO2s=',
       commitHash: process.env.NUXT_PUBLIC_COMMIT_HASH || 'dev',
-      version: '1.6.7',
+      version: '1.6.8',
     },
   },
   plugins: [{ src: '~/plugins/vue-query.client.ts', mode: 'client' }],
