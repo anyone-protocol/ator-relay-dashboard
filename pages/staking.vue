66be2c2d8acaacdea086c58f8982c0e8f99bea80
gregogun <gregogun97@gmail.com>
2025-05-15 17:19:12 +0100

    feat: staking ui improvements

--- pages/staking.vue
+++ pages/staking.vue
@@ -24,38 +24,79 @@
             icon: 'i-heroicons-circle-stack-20-solid',
             label: 'No operators.',
           }"
-          :rows="operators.value as Operator[]"
-          v-model="selected"
-          @select="select"
-          :single-select="true"
-        />
-
-        <div class="flex justify-end">
-          <form
-            @submit.prevent="submitStakeForm"
-            class="mt-6 md:mt-0 w-full md:w-auto"
-          >
-            <div class="text-gray-400 mb-2">Stake selected operator</div>
-            <div class="flex flex-col gap-2">
-              <UInput
-                v-model="stakeAmount"
-                color="neutral"
-                placeholder="Amount to stake"
-                type="number"
-                min="0"
-              />
+          :columns="columns"
+          :rows="operators"
+        >
+          <template #actions-data="{ row }">
+            <UDropdown :items="operatorActionItems(row)">
               <UButton
-                :loading="isSubmitting.value"
-                type="submit"
-                variant="outline"
-                color="cyan"
-                class="justify-center text-md"
-              >
-                {{ isPending ? 'Staking' : 'Stake' }}
-              </UButton>
+                color="neutral"
+                variant="ghost"
+                icon="i-heroicons-ellipsis-horizontal-20-solid"
+              />
+            </UDropdown>
+          </template>
+        </UTable>
+
+        <!-- Stake dialog -->
+        <UModal v-model="stakeDialogOpen">
+          <UCard
+            class="bg-white dark:bg-neutral-900 rounded-lg shadow-lg relative ring-0"
+          >
+            <div class="flex gap-2 mb-2 pb-4">
+              <Icon
+                name="i-heroicons-chart-pie-20-solid"
+                color="#24adc3"
+                class="h-6 w-auto"
+              ></Icon>
+
+              <h4 class="text-lg font-semibold">Stake with an operator</h4>
             </div>
-          </form>
-        </div>
+            <form
+              @submit.prevent="submitStakeForm"
+              class="mt-6 md:mt-0 w-full md:w-auto"
+            >
+              <div class="text-gray-400 mb-2">Staking with operator:</div>
+              <UInput
+                class="mb-2"
+                :disabled="true"
+                :model-value="selectedOperator?.operator"
+              />
+              <div class="flex flex-col gap-2">
+                <div class="text-gray-400">Amount to stake:</div>
+                <UInput
+                  v-model="stakeAmount"
+                  color="neutral"
+                  placeholder="Amount to stake"
+                  type="number"
+                  min="0"
+                />
+                <div class="flex justify-end gap-3 mt-5">
+                  <UButton
+                    @click="stakeDialogOpen = false"
+                    type="button"
+                    variant="outline"
+                    color="cyan"
+                    class="justify-center text-md"
+                  >
+                    Cancel
+                  </UButton>
+                  <UButton
+                    :loading="isSubmitting.value"
+                    type="submit"
+                    variant="solid"
+                    color="cyan"
+                    class="justify-center text-md"
+                  >
+                    {{ isPending ? 'Staking' : 'Stake' }}
+                  </UButton>
+                </div>
+              </div>
+            </form>
+          </UCard>
+        </UModal>
+
+        <div class="flex justify-end"></div>
       </template>
       <template #vault="{ item }">
         <p>{{ item.label }}</p>
@@ -76,11 +117,11 @@ import {
   hodlerAbi,
   tokenAbi,
 } from '../assets/contract-artifacts/wagmi-generated';
-import { parseEther } from 'viem';
+import { formatUnits, parseEther } from 'viem';
 
 interface Operator {
   operator: `0x${string}`;
-  amount: bigint;
+  amount: string;
 }
 
 const { address, isConnected } = useAccount();
@@ -94,9 +135,21 @@ const toast = useToast();
 const CONTRACT_ADDRESS = '0x948B3c65b89DF0B4894ABE91E6D02FE579834F8F';
 const OPERATOR_ADDRESS = '0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC';
 
+const stakeDialogOpen = ref(false);
 const stakeAmount = ref(0);
+const selectedOperator = ref<Operator | null>(null);
 const hodlerAddress = computed(() => address.value);
-const operators = computed(() => stakesData ?? []);
+const operators = computed(() => {
+  if (!stakesData.value) return [];
+  const stakes: Operator[] = stakesData.value.map((stake) => {
+    const amount = formatUnits(stake.amount, 18);
+    return {
+      operator: stake.operator,
+      amount,
+    };
+  });
+  return stakes;
+});
 const isSubmitting = computed(() => isPending || isConfirming);
 
 const {
@@ -147,6 +200,38 @@ const items = [
   },
 ];
 
+const columns = [
+  {
+    key: 'operator',
+    label: 'Address',
+  },
+  {
+    key: 'amount',
+    label: 'Amount staked',
+  },
+  {
+    key: 'actions',
+  },
+];
+
+const operatorActionItems = (row: Operator) => [
+  [
+    {
+      label: 'Stake',
+      icon: 'i-heroicons-chart-pie-20-solid',
+      click: () => [
+        (selectedOperator.value = row),
+        (stakeDialogOpen.value = true),
+      ],
+    },
+    {
+      label: 'Unstake',
+      icon: 'i-heroicons-minus-circle-20-solid',
+      click: () => console.log(`Unstaking with operator: ${row.operator}`),
+    },
+  ],
+];
+
 function select(row: Operator) {
   const index = selected.value.findIndex(
     (item) => item.operator === row.operator
@@ -157,17 +242,6 @@ function select(row: Operator) {
     selected.value = [];
   }
 }
-
-// const operators: Operator[] = [
-//   {
-//     operator: '0x667...953',
-//     amount: BigInt(100),
-//   },
-//   {
-//     operator: '0x667...959',
-//     amount: BigInt(100),
-//   },
-// ];
 
 const selected = ref<Operator[]>([]);
 
@@ -189,11 +263,11 @@ const submitStakeForm = async () => {
   }
 
   try {
-    console.log('parsing amount...');
+    // console.log('parsing amount...');
     const amount = parseEther(stakeAmount.value.toString());
 
     if (allowance.value === undefined || allowance.value < amount) {
-      console.log('Approving tokens...');
+      // console.log('Approving tokens...');
       await writeContractAsync({
         address: tokenAddress.value,
         abi: tokenAbi,
@@ -206,7 +280,7 @@ const submitStakeForm = async () => {
       console.log('Sufficient allowance, skipping approval');
     }
 
-    console.log('writing to contract...');
+    // console.log('writing to contract...');
     await writeContractAsync({
       address: CONTRACT_ADDRESS,
       abi: hodlerAbi,
