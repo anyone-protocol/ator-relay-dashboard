713ee1e4a28f05f0a90d92c204509fe10f1c623f
gregogun <gregogun97@gmail.com>
2025-05-27 21:24:13 +0100

    align footer button styles

--- components/ui-kit/ReportIssueButton.vue
+++ components/ui-kit/ReportIssueButton.vue
@@ -1,5 +1,10 @@
 <template>
-  <UButton color="gray" variant="outline" @click="onReportIssueClicked">
+  <UButton
+    color="gray"
+    variant="soft"
+    @click="onReportIssueClicked"
+    class="ring-1 ring-inset ring-neutral-200 dark:ring-neutral-700 bg-neutral-100 dark:bg-neutral-800/50 hover:dark:bg-neutral-800/25"
+  >
     <UIcon name="i-heroicons-exclamation-circle" />
     <div>
       <span>Report Issue</span>
