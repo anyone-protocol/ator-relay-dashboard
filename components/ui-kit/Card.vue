58bd8ea0fa04f2ef865ab9d89d93567c0a6825dd
gregogun <gregogun97@gmail.com>
2025-07-01 20:39:18 +0100

    add your relays section to homepage

--- components/ui-kit/Card.vue
+++ components/ui-kit/Card.vue
@@ -18,7 +18,7 @@ const onInView = ([{ isIntersecting }]: [{ isIntersecting: boolean }]) => {
   <div
     v-intersection-observer="[onInView, { threshold: 0.75 }]"
     :class="[isVisible ? 'opacity-100' : 'opacity-70']"
-    class="rounded-xl w-full relative group h-full border-2 border-neutral-200 hover:border-cyan-500 dark:border-neutral-800 dark:hover:border-cyan-500 lg:opacity-100"
+    class="rounded-xl w-full relative group min-h-full border-2 border-neutral-200 hover:border-cyan-500 dark:border-neutral-800 dark:hover:border-cyan-500 lg:opacity-100"
   >
     <!-- Inner -->
     <div
@@ -26,21 +26,19 @@ const onInView = ([{ isIntersecting }]: [{ isIntersecting: boolean }]) => {
         isVisible
           ? 'bg-white dark:bg-neutral-900'
           : 'bg-white/90 dark:bg-neutral-900/90',
+        noPadding ? 'p-0' : 'py-4 px-4 ',
       ]"
-      class="relative flex flex-col h-full rounded-xl overflow-hidden"
+      class="relative flex flex-col min-h-full rounded-xl overflow-hidden"
     >
       <h2
         v-if="title"
-        class="text-neutral-800 group-hover:text-cyan-600 dark:text-neutral-100 dark:group-hover:text-cyan-300 text-xl tracking-wide flex items-center gap-2 py-4 px-4 relative z-10 font-medium"
+        class="text-neutral-800 group-hover:text-cyan-600 dark:text-neutral-100 dark:group-hover:text-cyan-300 text-xl tracking-wide flex items-center gap-2 relative z-10 font-medium"
       >
         <Icon v-if="icon" :name="icon" />
         {{ title }}
       </h2>
       <!-- Padding -->
-      <div
-        :class="[noPadding ? 'p-0' : 'py-4 px-4 ']"
-        class="flex-col flex h-full"
-      >
+      <div class="flex-col flex h-full mt-4">
         <slot></slot>
       </div>
       <div
