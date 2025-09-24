dc762270fd925a10a926d2472b7f78fbc18a4cd3
Jim Toth <jim@artby.city>
2025-09-24 13:20:23 -0400

    Update version from 1.9.3 to 2.0.7 in nuxt.config.ts
    Signed-off-by: Jim Toth <jim@artby.city>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -84,7 +84,7 @@ export default defineNuxtConfig({
       supportWalletPublicKeyBase64:
         'K3jnSGVyHj4kSzgce3+k8gJsfHvUoQeJMNPO8CcsO2s=',
       commitHash: process.env.NUXT_PUBLIC_COMMIT_HASH || 'dev',
-      version: '1.9.3',
+      version: '2.0.7',
     },
   },
   plugins: [{ src: '~/plugins/vue-query.client.ts', mode: 'client' }],
