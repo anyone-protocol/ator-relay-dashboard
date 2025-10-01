40c082ef9b66598aac294e4b94d8bf042561a9f6
Jim Toth <jim@artby.city>
2025-10-01 17:23:56 -0400

    Bump in-app version to 2.0.9

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -84,7 +84,7 @@ export default defineNuxtConfig({
       supportWalletPublicKeyBase64:
         'K3jnSGVyHj4kSzgce3+k8gJsfHvUoQeJMNPO8CcsO2s=',
       commitHash: process.env.NUXT_PUBLIC_COMMIT_HASH || 'dev',
-      version: '2.0.7',
+      version: '2.0.9',
     },
   },
   plugins: [{ src: '~/plugins/vue-query.client.ts', mode: 'client' }],
