be751b8e4050bb19a9ed3500600e032ce76778d1
Jim Toth <jim@artby.city>
2025-10-29 14:11:56 -0400

    Fixes arweave deploy script & updates ANT process ID to latest

--- operations/scripts/deploy.mjs
+++ operations/scripts/deploy.mjs
@@ -1,18 +1,14 @@
 import { ANT, ArweaveSigner } from '@ar.io/sdk'
 import { TurboFactory } from '@ardrive/turbo-sdk'
-import { copyFileSync } from 'fs'
 
 import { logger } from './util/logger.mjs'
 
-// ator-dashboard process id
-// const processId = '9UpvN6H7sMo0vkuQeou3Ra0pZguWavtpt_8lfqPQlI8'
-
-// anyone process id
-// const processId = 'lZKDfIa5JiWQ8ojjXiRSey_81Bftbib4tLpIJh4UO0g'
-const processId = 'Rglgc93lGIpqHIgNo9rS0gIkG2s7aodCSMoGysi3r6Y'
+// ar://anyone process id (latest)
+const processId = 'dko67T5bT9J4KjNVy5kzEZDMLTTyZ71-rXE23ZOpCt0'
 const DEPLOY_FOLDER = `${process.cwd()}/.output/public`
 const gatewayUrl = process.env.GATEWAY || 'https://ar.anyone.tech'
 const url = process.env.BUNDLER || 'https://ar.anyone.tech/bundler'
+const permawebKey = process.env.PERMAWEB_KEY || 'NO_KEY'
 
 let undername = 'dev'
 if (process.env.PHASE === 'stage') {
@@ -24,9 +20,7 @@ if (process.env.PHASE === 'stage') {
 }
 
 async function deploy() {
-  const jwk = JSON.parse(
-    Buffer.from(process.env.PERMAWEB_KEY || 'NO_KEY', 'base64').toString('utf-8')
-  )
+  const jwk = JSON.parse(Buffer.from(permawebKey, 'base64').toString('utf-8'))
   const signer = new ArweaveSigner(jwk)
   const turbo = TurboFactory.authenticated({
     signer,
@@ -34,26 +28,29 @@ async function deploy() {
     uploadServiceConfig: { url, logger }
   })
   const ant = ANT.init({ processId, signer, logger })
-
-  // Hack: copy index.html to a file named "*" so it gets added to manifest as
-  //       a wildcard route
-  copyFileSync(DEPLOY_FOLDER + '/index.html', DEPLOY_FOLDER + '/*')
-
   const {
-    fileResponses,
     manifestResponse,
     manifest,
     errors
-  } = await turbo.uploadFolder({ folderPath: DEPLOY_FOLDER, dataItemOpts: { tags: [{ name: 'Deploy-Nonce', value: Date.now().toString() }] } })
+  } = await turbo.uploadFolder({
+    folderPath: DEPLOY_FOLDER,
+    dataItemOpts: {
+      tags: [{ name: 'Deploy-Nonce', value: Date.now().toString() }],
+    },
+    manifestOptions: {
+      indexFile: 'index.html',
+      fallbackFile: 'index.html'
+    }
+  })
 
   if (errors && errors.length > 0) {
     logger.error(errors)
     throw new Error('Deploy failed, see errors above')
   }
-  logger.info(`Manifest id ${manifestResponse?.id}`)
-  logger.info('Manifest', JSON.stringify(manifest))
+  logger.info(`Deployed to Arweave with manifest id: ${manifestResponse?.id}`)
+  logger.info('Deployed manifest:', JSON.stringify(manifest))
   logger.info('Updating ANT undername', undername)
-  const { id: deployedTxId } = undername === '@'
+  const { id: deployedMessageId } = undername === '@'
     ? await ant.setBaseNameRecord({
       transactionId: manifestResponse?.id,
       ttlSeconds: 3600
@@ -64,9 +61,12 @@ async function deploy() {
       ttlSeconds: 3600
     })
   logger.info(
-    'Deployed!  Please wait 20 - 30 minutes for ARNS to update!',
-    deployedTxId
+    `ANT updated! TTL 1 hour. View Set-Record message at `
+      + `https://ao.link/#/entity/${deployedMessageId}`
   )
 }
 
-deploy().catch(err => { logger.error('error deploying!', err); process.exit(1); })
+deploy().catch(err => {
+  logger.error('error deploying!', err)
+  process.exit(1)
+})
