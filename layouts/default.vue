5ceb0a5fb723fa39ec3493daab27cb0deceaa9b2
gregogun <gregogun97@gmail.com>
2025-12-10 10:52:03 +0000

    add mainnet migration warning banner

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,6 +11,15 @@ onMounted(() => {
   <main
     class="relative isolate h-full min-h-screen bg-neutral-50 dark:bg-neutral-900"
   >
+    <div
+      class="absolute top-0 w-full bg-yellow-400 text-black text-center text-xs font-medium py-1"
+    >
+      The Anyone Protocol Dashboard is currently migrating from Testnet to
+      Mainnet. Operators should not interact. See
+      <a class="underline" href="https://t.me/anyonedevelopment/181">
+        details
+      </a>
+    </div>
     <div class="xl:p-6">
       <div
         class="mx-auto w-full max-w-screen-2xl h-[calc(100vh-0px)] sm:h-[calc(100vh-50px)] lg:h-[calc(100vh-0px)] xl:h-[calc(100vh-50px)] xl:rounded-xl flex flex-col"
