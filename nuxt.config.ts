46f46fa27089d1cd6586cd91b2bdf63633f94a56
Jim Toth <jim@memeticblock.com>
2026-07-02 15:11:58 -0400

    Fixes default distribution deployer address in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -77,7 +77,7 @@ export default defineNuxtConfig({
       metricsDeployer: '53E8wWz8XkP9pGDQrgTi69GLAzZ6geX8bJckcifBr1Q',
       registratorContract: '0xBaDEfC9bFdD8bAf78D948E4C9dB76734A056e2BD',
       hodlerContract: '0x853B73e080293CE696653cA466Ff2c3aAD92992f',
-      distributionDeployer: 'G5tXjZS4SaMJNwOENZYgymvc6Zxp2Wws4Qf8wB0b',
+      distributionDeployer: 'G5tXjZS4SaMJNwOENZYgymvc6Zxp2Wws4Qf8wB0b-zA',
       warpGateway: 'https://gw.warp.cc/gateway/v2',
       centralizedMetricsAPI: 'https://api.ec.anyone.tech',
       dreNodeBaseUrl: 'https://dre.ec.anyone.tech',
